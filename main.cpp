c0232c053fa192628f0970e8bc147bc1f3996c43
mathigatti <mathigatti@gmail.com>
2015-04-19 00:17:53 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+# include < iostream >
+# include " correpocoyo.h "
+using namespace std ;
+int main ( int argc , char * argv [])
+{
+CorrePocoyo carrera ;
+cout <<  carrera.nuevoCorredor(const int& 3) << endl ;
+return 0;
+}
